a317381fed2d93bb2ed8ce4088815406e06e7282
MaryShapiro <Mashynya9@yandex.ru>
2023-04-18 12:30:34 +0300

    Находим вторую цифру трёхзначного числа

---
+++ Task_10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+/*
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int GetSecondDigit(int num)
+{
+    int result = (num / 10) % 10;
+    return result;
+}
+Console.WriteLine($"{num} -> {GetSecondDigit(num)}");
